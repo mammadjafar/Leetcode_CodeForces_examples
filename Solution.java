002e218d191adf124f2949ad4eff11ccb580d3b8
Mammad Jafarzade <mamed.cafer@gmail.com>
2019-09-02 14:57:02 +0200

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution {
+	
+	private static Set<Character> sc = new HashSet<Character>();
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String str = in.nextLine();
+		sc.add('\'');
+		sc.add('.');
+		sc.add('?');
+		sc.add('’');
+		String [] strs = str.split("-");
+		for(int i=0; i<strs.length; i++)
+			strs[i] = pigLatin(strs[i]);
+		str = String.join("-", strs);
+		System.out.println(str);
+		
+	}
+	
+	private static String pigLatin(String str) {
+		int len = str.length();
+		
+		if(str.length() < 1)
+			return "";
+
+		//dont touch way
+		if(str.length() >= 3 && str.substring(str.length()-3).equals("way"))
+			return str;
+		
+		// find capitals start to end
+		List<Integer> caps = new ArrayList();
+		for(int i=0; i<str.length(); i++)
+			if(isUpperCase(str.charAt(i)))
+				caps.add(i);
+		
+		str = str.toLowerCase();
+		String res;
+		if(isVowel(str.charAt(0))){
+			res = str + "way";
+		} else {
+			res = str.substring(1) + str.charAt(0) + "ay";
+		}
+		
+		res = removePunc(res);
+		for(int i=len-1; i>=0; i--) { // can't
+			if(isPunc(str.charAt(i))){
+				int k = res.length() - len +i;
+				res = res.substring(0, k+1) + str.charAt(i) + 
+						res.substring(k+1);
+			}
+		}
+		
+		char [] a = res.toCharArray();
+		for(int u:caps)
+			a[u] = (char) (((int) a[u]) - 32);
+		res = new String(a);
+		
+		return res;
+	}
+	
+	private static String removePunc(String str) {
+		for(char c:sc)
+			str = str.replace(c+"", "");
+		return str;
+	}
+	
+	private static boolean isPunc(char c) {
+		// add others.. or regex 
+		if(sc.contains(c))
+			return true;
+		return false;
+	}
+	
+	private static boolean isUpperCase(char c) {
+		int ci = (int) c;
+		if( ci >= 65 && ci <=90)
+			return true;
+		return false;
+	}
+	
+	private static boolean isVowel(char c) 
+	{ 
+	    return (c == 'A' || c == 'E' || c == 'I' ||  
+	            c == 'O' || c == 'U' || c == 'a' ||  
+	            c == 'e' || c == 'i' || c == 'o' || 
+	            c == 'u'); 
+	}
+
+}
