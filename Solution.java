49328ea58d8fad68cf12b3ac4236f56f12e56c78
Mammad Jafarzade <mamed.cafer@gmail.com>
2019-09-02 14:58:35 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -4,6 +4,26 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
+/*
+Pig Latin Task
+Write some Java code that translates a string (word, sentence, or paragraph) into “pig-latin” using the
+following rules.
+o Words that start with a consonant have their first letter moved to the end of the word and the
+letters “ay” added to the end.
+ Hello becomes Ellohay
+o Words that start with a vowel have the letters “way” added to the end.
+ apple becomes appleway
+o Words that end in “way” are not modified.
+ stairway stays as stairway
+o Punctuation must remain in the same relative place from the end of the word.
+ can’t becomes antca’y
+ end. becomes endway.
+o Hyphens are treated as two words
+ this-thing becomes histay-hingtay
+o Capitalization must remain in the same place.
+ Beach becomes Eachbay
+ McCloud becomes CcLoudmay*/
+
 public class Solution {
 	
 	private static Set<Character> sc = new HashSet<Character>();
